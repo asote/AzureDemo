9042fe638eb2af30d677357fad43db25d69f55ca
Antonio Sotelo <ASotelo6@gmail.com>
2017-02-25 14:14:33 -0600

    added resource group and storage account

--- data-variables.tf
+++ data-variables.tf
@@ -16,3 +16,18 @@ variable "azure-resourcegroup" {
   description = "Azure resource group name"
   default     = "${var.azure-region}-rg"
 }
+
+variable "azure-storageacc" {
+  description = "Azure storage account name."
+  default     = "${var.azure-resourcegroup}-sta"
+}
+
+variable "azure-storageacctype" {
+  description = "Azure storage account type."
+  default     = "Standard_GRS"
+}
+
+variable "azure-blobcontainer" {
+  description = "Azure storage blob container name."
+  default     = "vhds"
+}
--- main.tf
+++ main.tf
@@ -6,8 +6,28 @@
 
 provider "azurerm" {}
 
-# Create Azure resource group
+# Create Azure resource group.
 resource "azurerm_resource_group" "rg" {
   name     = "${var.azure-resourcegroup}"
   location = "${var.azure-region}"
+}
+
+# Create Azure storage account and container for VHDs.
+resource "azurerm_storage_account" "storage" {
+  name                = "${var.azure-storageacc}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  location     = "${azurerm_resource_group.rg.location}"
+  account_type = "${var.azure-storageacctype}"
+
+  tags {
+    environment = "test"
+  }
+}
+
+resource "azurerm_storage_container" "blob1" {
+  name                  = "${var.azure-blobcontainer}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  storage_account_name  = "${azurerm_storage_account.storage.name}"
+  container_access_type = "private"
 }
