e88447353e77263d6c0e365abc6c897596ef2642
Antonio Sotelo <ASotelo6@gmail.com>
2017-02-26 15:33:47 -0600

    added additional output

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,36 @@ output "appservers_ip" {
   value       = ["${azurerm_network_interface.app.*.private_ip_address}"]
 }
 
+output "dataservers_name" {
+  description = "data Servers names."
+  value       = ["${azurerm_virtual_machine.data.*.name}"]
+}
+
+output "dataservers_ip" {
+  description = "App Servers IP addresses."
+  value       = ["${azurerm_network_interface.data.*.private_ip_address}"]
+}
+
+output "addsservers_name" {
+  description = "adds Servers names."
+  value       = ["${azurerm_virtual_machine.adds.*.name}"]
+}
+
+output "addsservers_ip" {
+  description = "adds Servers IP addresses."
+  value       = ["${azurerm_network_interface.adds.*.private_ip_address}"]
+}
+
+output "mgtservers_name" {
+  description = "mgt Servers names."
+  value       = ["${azurerm_virtual_machine.mgt.*.name}"]
+}
+
+output "mgtservers_ip" {
+  description = "App Servers IP addresses."
+  value       = ["${azurerm_network_interface.mgt.*.private_ip_address}"]
+}
+
 output "LB_VIP_IP" {
   description = "Internet facing LB VIP."
   value       = ["${azurerm_public_ip.vip.ip_address}"]
