f35d0694bbc37c9b1e999f37bd5b56555c1ebb9b
Antonio Sotelo <ASotelo6@gmail.com>
2017-02-26 15:00:59 -0600

    added DCs

--- main.tf
+++ main.tf
@@ -802,3 +802,55 @@ resource "azurerm_availability_set" "adds" {
     environment = "${var.environment}"
   }
 }
+
+# Domain Controllers
+resource "azurerm_virtual_machine" "adds" {
+  count = "${var.adds-count}"
+  name  = "${var.addsserver-name}${count.index + 1}"
+
+  location = "${azurerm_resource_group.rg.location}"
+
+  resource_group_name              = "${azurerm_resource_group.rg.name}"
+  network_interface_ids            = ["${element(azurerm_network_interface.adds.*.id, count.index)}"]
+  availability_set_id              = "${azurerm_availability_set.adds.id}"
+  delete_os_disk_on_termination    = "true"
+  delete_data_disks_on_termination = "true"
+  vm_size                          = "${var.adds-vmsize}"
+
+  storage_image_reference {
+    publisher = "${var.addsimage-publisher}"
+    offer     = "${var.addsimage-offer}"
+    sku       = "${var.addsimage-sku}"
+    version   = "${var.addsimage-version}"
+  }
+
+  storage_os_disk {
+    name          = "osdisk${count.index}"
+    vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.blob.name}/${var.addsserver-name}${count.index + 1}-osdisk${count.index}.vhd"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  storage_data_disk {
+    name          = "datadisk${count.index}"
+    vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.blob.name}/${var.addsserver-name}${count.index + 1}-datadisk${count.index}.vhd"
+    disk_size_gb  = "${var.datadisk-size}"
+    create_option = "Empty"
+    lun           = 0
+  }
+
+  os_profile {
+    computer_name  = "${var.addsserver-name}${count.index + 1}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+
+  os_profile_windows_config {
+    enable_automatic_upgrades = "false"
+    provision_vm_agent        = "true"
+  }
+
+  tags {
+    environment = "${var.environment}"
+  }
+}
